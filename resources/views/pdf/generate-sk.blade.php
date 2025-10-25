161cb6663f239843eb29363a3556339d33904cb3
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-10-26 00:27:55 +0800

    Feat: Update template PDF SK

--- resources/views/pdf/generate-sk.blade.php
+++ resources/views/pdf/generate-sk.blade.php
@@ -6,12 +6,12 @@
     <style>
         @font-face {
             font-family: 'xd-prime-regular';
-            src: url("{{ base_path('public/assets/font/xd-prime/XDPrime-Regular.ttf') }}") format('truetype');
+            src: url("{{ public_path('assets/font/xd-prime/XDPrime-Regular.ttf') }}") format('truetype');
         }
 
         @font-face {
-            font-family: 'xd-prime-medium';
-            src: url("{{ base_path('public/assets/font/xd-prime/XDPrime-Medium.ttf') }}") format('truetype');
+            font-family: 'xd-prime-bold';
+            src: url("{{ public_path('assets/font/xd-prime/XDPrime-Bold.ttf') }}") format('truetype');
         }
 
         * {
@@ -21,7 +21,7 @@
 </head>
 <body>
 <img src="{{public_path('assets/image/event/' . $event->image_path)}}" alt="Logo Path" style="width: 120px !important; height: 120px !important;">
-<h4 style="text-transform: uppercase; text-align: center;">STRUKTUR KEPANITIAAN <br> {{$event->name}}</h4>
+<p style="text-transform: uppercase; text-align: center; font-family: 'xd-prime-bold' !important;">STRUKTUR KEPANITIAAN <br> {{$event->name}}</p>
 
 <table>
     <tr>
@@ -51,6 +51,21 @@
             <td style="font-size: 0.875rem; padding: 6px; border: 1px solid black;">{{$eventRecruitment->student_code}}</td>
         </tr>
     @endforeach
+</table>
+
+<table style="width: 100%; margin-top: 120px;" cellpadding="0" cellspacing="0">
+    <tr>
+        <td style="width: 80%;"></td>
+        <td style="width: 80%;"></td>
+        <td style="width: 100%;">
+            <div style="position: relative;">
+                <p style="position: relative; font-size: 0.913rem; line-height: 100%; margin: 0 !important; padding: 0; width: fit-content !important;">Ketua Steering Committee,</p>
+                <p style="position: relative; font-size: 0.913rem; line-height: 100%; margin: 0 !important; padding: 0; width: fit-content !important;">Kepala Program Studi Sistem Informasi</p>
+                <p style="position: relative; font-size: 0.913rem; line-height: 100%; margin: 0 !important; margin-top: 80px !important; padding: 0; width: fit-content !important;">{{$event->steering_committee_chair}}</p>
+                <img src="{{public_path('assets/image/sample-ttd.png')}}" alt="Sample TTD" style="width: 240px !important; position: absolute; bottom: 88%; left: -10%;">
+            </div>
+        </td>
+    </tr>
 </table>
 </body>
 </html>
